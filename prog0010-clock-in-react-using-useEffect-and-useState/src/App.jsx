c86f36a6a8156716f0f15c6e318c70fe9bf02e4b
RonxldWilson <ronaldwilson00000@gmail.com>
2024-09-30 11:13:04 +0530

    Clock in React (completed)

--- prog0010-clock-in-react-using-useEffect-and-useState/src/App.jsx
+++ prog0010-clock-in-react-using-useEffect-and-useState/src/App.jsx
@@ -1,16 +1,37 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState("")
+  const [time, setTime] = useState("")
 
-  
+  useEffect(()=>{
+    setInterval( ()=>{
+      const date = new Date();
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+      let seconds = date.getSeconds();
+
+      let timeDisplay = hours + " : " + minutes + " : " + seconds;
+      
+      // if(hours > 12)
+      console.log(timeDisplay)
+      setTime((time)=> time = timeDisplay)
+
+    },1000)
+  },[])
 
   return (
     <>
-      Hello world
+      <Clock time={time}></Clock>
     </>
   )
+}
+
+function Clock({time}){
+  return <>
+    {time}
+  </>
 }
 
 export default App
