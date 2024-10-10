60a84c9a5e2873e01820188dbbbd7fb6c89ceb0e
RonxldWilson <ronaldwilson00000@gmail.com>
2024-10-10 10:34:37 +0530

    Card creation in React JS

---
+++ prog0022-assignment-for-profile-card-component/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Card } from './components/Card'
+
+function App() {
+
+  return (
+    <>
+    <div style={{display:'flex'}}>
+      <Card name="Ron" age="21" location="Bengaluru" followerCount="123" likeCount="123" photoCount="123" />
+      <Card name="Ron" age="21" location="Bengaluru" followerCount="123" likeCount="123" photoCount="123" />
+      <Card name="Ron" age="21" location="Bengaluru" followerCount="123" likeCount="123" photoCount="123" />
+      <Card name="Ron" age="21" location="Bengaluru" followerCount="123" likeCount="123" photoCount="123" />
+      <Card name="Ron" age="21" location="Bengaluru" followerCount="123" likeCount="123" photoCount="123" />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ prog0022-assignment-for-profile-card-component/src/components/Card.jsx
@@ -0,0 +1,25 @@
+import image from '../assets/image.png';
+
+//  name="" age="" location="" followerCount="" likeCount="" photoCount=""
+export function Card({name,age,location,followerCount,likeCount,photoCount}){
+    return <>
+    <div style={{background: "grey", color:"black", width:600, height: 600, margin:20}}>
+        <div style={{background: "teal", width:600, height: 450}}>
+            <div style={{display: 'flex', justifyContent: 'center', alignItems:'center', paddingTop:100}}>
+                <div>
+                    <img src={image} alt="profile-image" width={200} height={200} style={{background:"yellow", borderRadius: "50%" }} />
+            <div><b>{name}</b> {age}</div>
+            <div> {location} </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div style={{display: "flex", alignItems:"center" ,justifyContent:"space-around"}}>
+                <div style={{padding: 40}}> <b> {followerCount} </b> <br />Followers </div>
+                <div style={{padding: 40}}> <b> {likeCount} </b> <br /> Likes </div>
+                <div style={{padding: 40}}> <b> {photoCount} </b> <br /> Photos </div>
+            </div>
+        </div>
+    </div>
+    </>
+}
